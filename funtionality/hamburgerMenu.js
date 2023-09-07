0fc3092bd6c080f4b578c1090025fbdf2b0ee479
Ladipo Deju <ladipodeju@gmail.com>
2023-09-08 00:02:44 +0100

    Change toggleMenu() to arrow function, from function declaration

--- funtionality/hamburgerMenu.js
+++ funtionality/hamburgerMenu.js
@@ -1,11 +1,4 @@
-/*const toggleMenu = () => {
-    const menu = document.getElementById("menu-links");
-    const icon = document.getElementById("hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}*/
-
-function toggleMenu() {
+const toggleMenu = () => {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
